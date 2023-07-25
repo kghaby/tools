bd537407b99505dd730d084e1ff0308f1f1ddfe3
kghaby <kyleghaby@gmail.com>
2023-07-24 20:51:24 -0500

    lots

--- tools/md/amber/mm/md/linked/US/US_3/analysis/make_meta.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm meta.dat
-
-k="0.12184" #units: kcal/mol/deg^2. converted from 200 kcal/mol/rad^2
-
-for i in `seq -10 4 150`;do 
-echo "../US_$i/dihed_$i.dat 	$i.0 	$k" >> meta.dat
-done  
--- tools/md/amber/mm/md/unlinked/US/US_6/analysis/run_wham.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-~/utilities/wham/wham/wham/wham P -11 151 41 0.01 310 0 meta.dat pmf.dat
-
- 
-
