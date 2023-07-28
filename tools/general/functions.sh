03743c90fc73951377bd7f6f281c24441cab1093
kghaby <61133492+kghaby@users.noreply.github.com>
2023-07-27 20:22:54 -0500

    Update functions.sh

--- tools/general/functions.sh
+++ tools/general/functions.sh
@@ -1,24 +1,26 @@
+#!/bin/bash
 #random functions that are useful in scripts
+
 
 linker () {
     mkdir .lsp_symlink
     cd .lsp_symlink
-    ln -s $1 $2
+    ln -s "$1" "$2"
 }
 
 testfile () {
-    echo "#!/bin/bash" > $1
-    echo "set -e" >> $1
-    chmod a+x $1
-    vim $1
+    echo "#!/bin/bash" > "$1"
+    echo "set -e" >> "$1"
+    chmod a+x "$1"
+    vim "$1"
 }
 
 #function for connecting to remote jupyter-lab
 jllocal() {
   rport=27648
   lport=27648
-  remote_username=$1 #eg kyleghaby
-  remote_hostname=$2 #eg beagle3.rcc.uchicago.edu
+  remote_username="$1" #eg kyleghaby
+  remote_hostname="$2" #eg beagle3.rcc.uchicago.edu
   url="http://localhost:$lport"
   echo "URL: $url"
   #open "$url"
@@ -29,7 +31,7 @@ jllocal() {
 
 #compares 2 files
 comparetxt() {
-    sdiff $1 $2 | more
+    sdiff "$1" "$2" | more
 }
 
 addignore() {
@@ -37,9 +39,8 @@ addignore() {
     find . -depth -not -path "*target*" -not -path "./.git/*" -type d -empty -print
     find . -depth -not -path "*target*" -not -path "./.git/*" -type d -empty -exec touch {}/.gitignore \;
 }
-
 verify_target() {
-    local target=$1
+    local target="$1"
 
     # check whitelist
     if (( ${#whitelist[@]} > 0 )); then
@@ -67,23 +68,20 @@ verify_target() {
     return 0
 }
 
-
 # Function to create a label from a directory path
 label_target() {
-    local target_path=$1
-    local dirs=$2
+    local target_path="$1"
+    local targets="$2"
     IFS='/' read -ra target_parts <<< "$target_path"
-    IFS='/' read -ra dirs_parts <<< "$dirs"
-    
+    IFS='/' read -ra targets_parts <<< "$targets"
+
     local label_parts=()
-    for index in "${!dirs_parts[@]}"; do
-        if [[ "${dirs_parts[index]}" == "*" ]]; then
+    for index in "${!targets_parts[@]}"; do
+        if [[ "${targets_parts[index]}" == "*" ]]; then
             label_parts+=("${target_parts[index]}")
         fi
     done
 
     local label=$(IFS=_; echo "${label_parts[*]}")
-    echo $label
+    echo "$label"
 }
-
-
