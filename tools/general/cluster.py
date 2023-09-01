503e1a0fe6fb1035443ce0cdb791e4ad39d41edd
kyleghaby <kyleghaby123@yahoo.com>
2023-09-01 16:57:13 -0500

    f

--- tools/general/cluster.py
+++ tools/general/cluster.py
@@ -44,11 +44,15 @@ def elbow_method(data, k_range):
     rate_change = np.diff(np.diff(inertia))
     return np.argmax(rate_change) + k_range[0] + 1
 
-def batch_std(arr, batch_size=1000):
-    n = len(arr)
-    batches = [arr[i:i + batch_size] for i in range(0, n, batch_size)]
-    squared_diff_sum = np.sum([np.sum((batch - np.mean(batch))**2) for batch in batches])
-    return np.sqrt(squared_diff_sum / (n - 1))
+def incremental_std(cluster_data):
+    n = len(cluster_data)
+    squared_diff_sum = 0
+    
+    for point in cluster_data:
+        squared_diff_sum += np.sum((point - cluster_data) ** 2)
+        
+    return np.sqrt(squared_diff_sum / (n * (n - 1)))  # n * (n - 1) because of pairwise comparisons
+
 
 def cluster_summary(data, labels, centroids):
     unique_labels = np.unique(labels)
@@ -61,10 +65,7 @@ def cluster_summary(data, labels, centroids):
         fraction = n_cluster_frames / n_frames
         avg_dist = np.mean([np.mean(np.abs(point - cluster_data)) for point in cluster_data])
 
-        # Revised Stdev calculation with batching
-        distances = [np.abs(point - cluster_data) for point in cluster_data]
-        flattened_distances = np.concatenate(distances)
-        stdev = batch_std(flattened_distances)
+        stdev = incremental_std(cluster_data)
 
         
         # Centroid calculation
